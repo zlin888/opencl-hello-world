607c8b5211d8b8ee223d3b74ece75268ee6e55ca
zlin888 <zhitaolin@outlook.com>
2020-09-06 17:14:50 +0800

    dev: read jpeg

--- convolution/main.cpp
+++ convolution/main.cpp
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <math.h>
 #include "kernel.hpp" //const char* kernel_source is defined in here
+#include <jpeg_loader.hpp>
+#include <iostream>
 
 using namespace std;
 using namespace utils;
@@ -23,7 +25,10 @@ public:
 
 void ConvolutionCL::runKernel()
 {
-    
+    JpegLoader jpeg_loader;
+    const JpegLoader::ImageInfo *img_info = jpeg_loader.Load("../assets/cat-640-426.jpeg");
+
+
 }
 
 int main(int argc, char **argvs)
