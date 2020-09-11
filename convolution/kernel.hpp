f539e033a1d921816dcac251a300a36797ba387a
zlin888 <zhitaolin@outlook.com>
2020-09-11 11:26:34 +0800

    dev: finish kernel (9 to 1, will shrink the src img)

--- convolution/kernel.hpp
+++ convolution/kernel.hpp
@@ -10,24 +10,15 @@
 
 const char *kernel_name = "convolution";
 std::string kernel_source = STRINGIFY(
-    __kernel void convolution(__global float *src_data, __global float *dest_data, int W, int H) //Rotation Parameters
+    __kernel void convolution(__global float *src_data, __global float *dest_data, int W, int H, int Wd, int Hd) //Rotation Parameters
     {
         //Work-item gets its index within index space
         const int xi = get_global_id(0);
         const int yi = get_global_id(1);
 
-        float x0 = W / 2.0f;
-        float y0 = H / 2.0f;
-        float delta_x = xi - x0;
-        float delta_y = yi - y0;
-
-        int xpos = (int)(delta_x * cosTheta + delta_y * sinTheta + x0);
-        int ypos = (int)(delta_y * cosTheta + delta_x * sinTheta + y0);
-
-        //Bound Checking
-        if (((int)xpos >= 0) && ((int)xpos < W) &&
-            ((int)ypos >= 0) && ((int)ypos < H))
-        {
-            dest_data[yi * W + xi] = src_data[ypos * W + xpos];
+        if ((xi + 2) % 3 == 0 && (yi + 2) % 3 == 0) {
+            int xd = (xi + 2) / 3 - 1;
+            int yd = (yi + 2) / 3 - 1;
+            dest_data[yd * Wd + xd] = src_data[yi * W + xi];
         }
     });
--- convolution/main.cpp
+++ convolution/main.cpp
@@ -25,27 +25,65 @@ public:
     using myutils::CL::CL;
     void prepare();
     void runKernel();
-    uint32_t img_size;
     Mat image;
 };
 
 void ConvolutionCL::prepare()
 {
     image = imread("../assets/cat.jpeg");
-    myutils::displayImage(image);
+    // myutils::displayImage(image);
 }
 
 void ConvolutionCL::runKernel()
 {
     prepare();
+    int H = image.size[0];
+    int W = image.size[1];
+    int Wd = floor(W / 3);
+    int Hd = floor(H / 3);
+    int img_size = H * W;
+
     auto inputBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY, img_size * sizeof(uint8_t), NULL, &this->err);
-    auto outputBuffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, img_size * sizeof(uint8_t), NULL, &this->err);
+    auto outputBuffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, Wd * Hd * sizeof(uint8_t), NULL, &this->err);
 
-    // this->err = clEnqueueWriteBuffer(this->commandQueue, inputBuffer, CL_TRUE, 0, img_size * sizeof(uint8_t), img_info->pData, 0, NULL, NULL);
-    // printf("enqueue buffer: %s\n", oclErrorString(this->err));
+    this->err = clEnqueueWriteBuffer(this->commandQueue, inputBuffer, CL_TRUE, 0, img_size * sizeof(uint8_t), image.data, 0, NULL, NULL);
+    printf("enqueue buffer: %s\n", oclErrorString(this->err));
 
-    // this->err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&inputBuffer);
-    // printf("set arg0: %s\n", oclErrorString(this->err));
+    this->err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&inputBuffer);
+    printf("set arg0: %s\n", oclErrorString(this->err));
+    this->err = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&outputBuffer);
+    printf("set arg1: %s\n", oclErrorString(this->err));
+    this->err = clSetKernelArg(kernel, 2, sizeof(int), (void *)&W);
+    printf("set arg2: %s\n", oclErrorString(this->err));
+    this->err = clSetKernelArg(kernel, 3, sizeof(int), (void *)&H);
+    printf("set arg3: %s\n", oclErrorString(this->err));
+    this->err = clSetKernelArg(kernel, 4, sizeof(int), (void *)&Wd);
+    printf("set arg4: %s\n", oclErrorString(this->err));
+    this->err = clSetKernelArg(kernel, 5, sizeof(int), (void *)&Hd);
+    printf("set arg5: %s\n", oclErrorString(this->err));
+
+    const size_t globalWorkSize[2] = {(size_t)W, (size_t)H};
+    cl_event event;
+    printf("in runKernel\n");
+    this->err = clEnqueueNDRangeKernel(commandQueue, kernel, 2, NULL, globalWorkSize, NULL, 0, NULL, &event);
+    printf("clEnqueueNDRangeKernel: %s\n", oclErrorString(this->err));
+
+    //Wait for the command queue to finish these commands before proceeding
+    clFinish(commandQueue);
+
+    myutils::printRuntimeInfo(event);
+    //lets check our calculations by reading from the device memory and printing out the results
+    float output[W * H];
+    this->err = clEnqueueReadBuffer(commandQueue, outputBuffer, CL_TRUE, 0, sizeof(uint8_t) * Wd * Hd, &output, 0, NULL, NULL);
+    printf("clEnqueueReadBuffer: %s\n", oclErrorString(this->err));
+
+    myutils::printMatrix(Wd, Hd, output);
+
+    if (inputBuffer)
+        clReleaseMemObject(inputBuffer);
+    if (outputBuffer)
+        clReleaseMemObject(outputBuffer);
+    printf("finish!\n");
 }
 
 int main(int argc, char **argvs)
